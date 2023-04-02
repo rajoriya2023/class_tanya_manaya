d6dbaa52026daa59e8da65440159cd17be141219
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-04-02 22:59:49 +0530

    Initial commit

---
+++ src/Tanaya_Manaya.java
@@ -0,0 +1,15 @@
+public class Tanaya_Manaya {
+    public static void main(String[]args){
+        System.out.println("tanya hi");
+        System.out.println("manaya hello");
+        System.out.println("tanya why are you not came to me");
+        System.out.println("manaya i was in my school");
+        System.out.println(" tanya ok but you looking to good");
+        System.out.println(" manaya oh thanks");
+        System.out.println(" tanaya but you also looking to good");
+        System.out.println("tanya where is sona didi");
+        System.out.println("manaya she will come after few hour");
+        System.out.println("manaya because see is in her coaching");
+
+    }
+}
